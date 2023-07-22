41fd0e1ba2b4063b2dfa1610dbcce4ab74c69250
mkumm14 <madhavkummara@outlook.com>
2023-07-21 22:14:13 -0500

    finished the sign up and login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,95 @@
-export default function LoginPage()
-{
+"use client"
+
+import Link from "next/link"
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import {useForm} from "react-hook-form";
+
+const formSchema = z.object({
+    email: z.string().nonempty({
+        message:'This is required'
+    }).email({
+        message:"Invalid email"
+    }),
+    password: z.string().nonempty({
+        message:'This is required'
+    })
+})
+
+export default function LoginPage() {
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
     return (
-        <h1>Login Page</h1>
+
+        <>
+
+            <div className={'flex justify-end items-center m-5 space-x-3'}>
+                <p className={''}>Already have an account?</p>
+                <Button asChild>
+                    <Link href={'/signup'}>Sign up</Link>
+                </Button>
+            </div>
+
+            <div className="flex items-center justify-center flex-col mt-10 mb-10 md:mb-0">
+                <p className={'text-2xl mb-5 font-bold underline'}>Login</p>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-64">
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="email" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="password" type={'password'} {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit">Submit</Button>
+                    </form>
+                </Form>
+            </div>
+        </>
     )
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,10 +1,113 @@
-import * as z from 'zod'
+"use client"
 
+import Link from "next/link"
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
 
-export default function SignUpPage()
-{
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import {useForm} from "react-hook-form";
+
+const formSchema = z.object({
+    username: z.string().min(2, {
+        message: "Username must be at least 2 characters.",
+    }),
+    email: z.string().email({
+        message:"Invalid email."
+    }),
+    password: z.string().min(5,{
+        message: "Username must be at least 5 characters."
+    })
+})
+
+export default function SignUpPage() {
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+            email:"",
+            password: "",
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
     return (
-        <h1>sign up page</h1>
+
+        <>
+
+            <div className={'flex justify-end items-center m-5 space-x-3'}>
+                <p className={''}>don&apos;t have an account?</p>
+                <Button asChild>
+                    <Link href={'/login'}>Login</Link>
+                </Button>
+            </div>
+
+        <div className="flex items-center justify-center flex-col mt-10 mb-10 md:mb-0">
+            <p className={'text-2xl mb-5 font-bold underline'}>Sign Up</p>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                                <Input placeholder="username" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                Username should be 3 characters long.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input placeholder="email" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input placeholder="password" type={'password'} {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+        </div>
+        </>
     )
 }
-
